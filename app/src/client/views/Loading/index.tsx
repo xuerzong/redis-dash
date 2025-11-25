700bcb47c8168156acfd6268f296b1b219c84d25
xuerzong <xuerzong@gmail.com>
2025-11-25 10:09:47 +0800

    Fix: Remove Settings Page
    No need settings page for now

--- app/src/client/views/Loading/index.tsx
+++ app/src/client/views/Loading/index.tsx
@@ -1,8 +1,24 @@
 import { useEffect } from 'react'
+import { useNavigate } from 'react-router'
+import { Loader } from '@/client/components/Loader'
+import { Box } from '@/client/components/ui/Box'
 
 const Page = () => {
-  useEffect(() => {})
-  return <div>loading...</div>
+  const navigate = useNavigate()
+  useEffect(() => {
+    navigate('/', { replace: true })
+  }, [])
+  return (
+    <Box
+      width="100vw"
+      height="100vh"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Loader />
+    </Box>
+  )
 }
 
 export default Page
