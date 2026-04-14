f6c2ba92eba57fdc876cbf27777e015d9a742185
xuerzong <xuerzong@gmail.com>
2026-04-15 00:04:57 +0800

    fix release version replacement

--- scripts/replace-version.mjs
+++ scripts/replace-version.mjs
@@ -10,6 +10,21 @@ if (!version) {
   throw new Error('RDS_VERSION environment variable not set')
 }
 
+const rewriteCargoPackageVersion = (filePath) => {
+  const content = fs.readFileSync(filePath, 'utf8')
+  const nextContent = content.replace(
+    /(^\[package\][\s\S]*?^version = ")([^"]+)(")/m,
+    `$1${version}$3`
+  )
+
+  if (nextContent === content) {
+    throw new Error(`Failed to update package version in ${filePath}`)
+  }
+
+  console.log('Writing version ' + version + ' to ' + filePath)
+  fs.writeFileSync(filePath, nextContent)
+}
+
 // Rewrite tauri version
 const tauriConfigPath = path.join(__dirname, '../desktop/tauri.conf.json')
 const tauriConfig = JSON.parse(fs.readFileSync(tauriConfigPath, 'utf8'))
@@ -18,3 +33,9 @@ tauriConfig.version = version
 
 console.log('Writing version ' + version + ' to ' + tauriConfigPath)
 fs.writeFileSync(tauriConfigPath, JSON.stringify(tauriConfig, null, 2))
+
+rewriteCargoPackageVersion(path.join(__dirname, '../desktop/Cargo.toml'))
+rewriteCargoPackageVersion(path.join(__dirname, '../native/rds/Cargo.toml'))
+rewriteCargoPackageVersion(
+  path.join(__dirname, '../native/rds-core/Cargo.toml')
+)
